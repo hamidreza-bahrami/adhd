098c6a16aaff626257d5813329b8c06eeaa53e93
hamidreza-bahrami <150763463+hamidreza-bahrami@users.noreply.github.com>
2024-01-06 19:21:43 +0330

    Update adhd.py

--- adhd.py
+++ adhd.py
@@ -84,9 +84,9 @@ def show_page():
 
         y_prediction = model.predict(x)
         if y_prediction == True:
-            st.write("<h4 style='text-align: center; color: gray;'>بر اساس داده های وارد شده، شما دارای بیش فعالی و نقص توجه هستید</h4>", unsafe_allow_html=True)
-            st.write("<h5 style='text-align: center; color: gray;'>برای درمان به روانشناس مراجعه کنید</h5>", unsafe_allow_html=True)
+            st.write("<h4 style='text-align: right; color: gray;'>بر اساس داده های وارد شده، شما دارای بیش فعالی و نقص توجه هستید</h4>", unsafe_allow_html=True)
+            st.write("<h5 style='text-align: right; color: gray;'>برای درمان به روانشناس مراجعه کنید</h5>", unsafe_allow_html=True)
         elif y_prediction == False:
-            st.write("<h4 style='text-align: center; color: gray;'>بر اساس داده های وارد شده، شما در سلامتی کامل هستید</h4>", unsafe_allow_html=True)
+            st.write("<h4 style='text-align: right; color: gray;'>بر اساس داده های وارد شده، شما در سلامتی کامل هستید</h4>", unsafe_allow_html=True)
 
 show_page()
